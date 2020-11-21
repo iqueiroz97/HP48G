9ee9fc98adead61a15f130fe3bc069a0c6657ba7
Igor Pereira de Queiroz <suporte7@sweda.sp>
2020-11-21 17:31:30 -0300

    implementação de filtro de entrada (operador)

--- calc.py
+++ calc.py
@@ -2,13 +2,15 @@
 valores = []
 vInt = False
 quantDig = 0
-operacoes = ["Soma", "Subtração", "Multiplicação", "Divisão"]
+operacoes = ["1 - Soma", "2 - Subtração", "3 - Multiplicação", "4 - Divisão"]
 operacao = 0
 valorFinal = 0
-l = 0
+lV = 0
+lO = 0
+operadorVal = False
 
-#filtro de entrada
-while (vInt == False):
+#filtro de entrada (valor)
+while vInt == False:
     quantDig = input("Quantos valores possui a operação?: ")
 
     if quantDig == "0" or quantDig == "1":
@@ -18,7 +20,6 @@ while (vInt == False):
         try: 
             int(quantDig)
             quantDig = int(quantDig)
-            operacao = input("Qual operação deseja realizar?: ")
             vInt = True
 
         except ValueError:
@@ -29,26 +30,52 @@ while (vInt == False):
             except ValueError:
                 print("Valor inválido. Insira um novo valor.")
 
+#mostra as operações
+while lO < 4:
+    print(operacoes.pop(0))
+    lO+=1
+    
+#filtro de entrada (operador) - não finalizado
+while operadorVal == False:
+    operacao = input("Insira o valor da operação que deseja realizar: ")
+
+    if operacao != "1" or operacao != "2" or operacao != "3" or operacao != "4":
+        print("Valor inválido. Insira um novo valor.")
+
+    else:
+        try:
+            int(operacao)
+            operacao = int(operacao)
+            operadorVal = True
+
+        except ValueError:
+            try:
+                float(operacao)
+                print("Valor inválido. Insira um novo valor.")
+
+            except ValueError:
+                print("Valor inválido. Insira um novo valor.")
+
 #salva valores digitados no array
-while l < quantDig:
-    valorN = input("Insira o " + str(l + 1) + "° valor: ")
+while lV < quantDig:
+    valorN = input("Insira o " + str(lV + 1) + "° valor: ")
 
     try: 
         int(valorN)
         valorN = int(valorN)
         valores.append(valorN)
-        l+=1
+        lV+=1
 
     except ValueError:
         try:
             float(valorN)
             valorN = float(valorN)
             valores.append(valorN)
-            l+=1
+            lV+=1
 
         except ValueError:
             print("Valor inválido. Insira um novo valor.")
 
 #printa valores em tela
-#print("O comprimento do array é " + str(len(valores)))
-print(valores)
+print("O comprimento do array é " + str(len(valores)))
+print("Valores: " + str(valores))
